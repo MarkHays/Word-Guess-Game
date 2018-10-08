3bc91409a51e66f924d1469c5ca7cc51cb3f7a7a
MarkHays <mark.hays1991@gmail.com>
2018-10-08 14:04:01 -0700

    another commit to fix bugs, game almost finished. just need to add the function that changes random letter after correct one is chosen.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,42 +4,54 @@ var computerGuess = alphabet[numberGuess];
 var wins = 0;
 var guessesLeft = 9;
 var losses = 0;
-var letterToGuess = null;
+var guessedLetters = [];
+
 
 
 console.log(computerGuess);
 document.onkeyup = function (event) {
+ var  userGuess = event.key;
+   console.log(userGuess);
+
+//var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+ 
+
+   var reset = function () {
+    totalGuesses = 0;
+    guessesLeft = 9;
+    guessedLetters = [];
+   
+    
+
+
+}
+
     //  alert(event.key)
     if (computerGuess === event.key) {
         wins++;
+        reset();
         document.getElementById('wins').innerText = wins;
 
-    } else {
-        --guessesLeft;
-        if (guessesLeft === 0) {
-            losses++;
-        }
 
-        document.getElementById("guessesLeft").innerText = guessesLeft;
+
+    } else {
+        guessesLeft--;
+        document.getElementById('guessesLeft').innerText = guessesLeft;
+        guessedLetters.push(userGuess);
+        document.getElementById('guessesSoFar').innerHTML = guessedLetters.join(", ");
+
+        }
+    if (guessesLeft === 0) {
+    losses++;
+        reset();
+    document.getElementById('losses').innerText = losses;
+
 
     }
 
- if (guesses === 0) {
-    guesses = 9;
-    losses++;
-    guessedLetters = [];
-}
+
+
+
+
 
 }
-var reset = function() {
-    totalGuesses = 9;
-    guessesLeft = 9;
-    guessedLetters = [];
-
-    updateLetterToGuess();
-    updateGuessesLeft();
-    updateGuessesSoFar();
-}
-
-updateLetterToGuess();
-updateGuessesLeft();
