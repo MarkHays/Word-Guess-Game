4c0f28ae64671350c4456380188d7f8737e173a4
MarkHays <mark.hays1991@gmail.com>
2018-10-08 19:55:15 -0700

    this is final commit game fully functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,52 @@
-var numberGuess = Math.floor(Math.random() * 26);
+
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var computerGuess = alphabet[numberGuess];
+
 var wins = 0;
 var guessesLeft = 9;
 var losses = 0;
 var guessedLetters = [];
 
-
-
+function randomLetter() {
+    var numberGuess = Math.floor(Math.random() * 26);
+    return alphabet[numberGuess];
+}
+var computerGuess = randomLetter();
 console.log(computerGuess);
 document.onkeyup = function (event) {
- var  userGuess = event.key;
-   console.log(userGuess);
+    var userGuess = event.key;
+    console.log(userGuess);
 
-//var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
- 
-
-   var reset = function () {
-    totalGuesses = 0;
-    guessesLeft = 9;
-    guessedLetters = [];
-   
-    
+    //var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 
 
-}
+    var reset = function () {
+        computerGuess = randomLetter();
+        console.log(computerGuess);
+        totalGuesses = 0;
+        guessesLeft = 9;
+        guessedLetters = [];
+    }
 
     //  alert(event.key)
     if (computerGuess === event.key) {
         wins++;
         reset();
         document.getElementById('wins').innerText = wins;
-
-
-
     } else {
         guessesLeft--;
         document.getElementById('guessesLeft').innerText = guessesLeft;
         guessedLetters.push(userGuess);
         document.getElementById('guessesSoFar').innerHTML = guessedLetters.join(", ");
-
-        }
+    }
     if (guessesLeft === 0) {
-    losses++;
+        losses++;
         reset();
-    document.getElementById('losses').innerText = losses;
+        document.getElementById('losses').innerText = losses;
 
 
     }
+
+
 
 
 
